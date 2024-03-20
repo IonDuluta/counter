8f77d25b5a4a986f8152694cbe57ed24c229cfe1
Duluta Ion <130406100+IonDuluta@users.noreply.github.com>
2024-03-20 10:40:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const number = document.getElementById('number');
+const buttonPlus = document.getElementById('plus');
+const buttonMinus = document.getElementById('minus');
+const buttonClear = document.getElementById('clear');
+
+buttonPlus.addEventListener('click', add);
+buttonMinus.addEventListener('click', subtract);
+buttonClear.addEventListener('click', clear);
+
+let value = 0;
+
+function add() {
+    value++;
+    number.innerHTML = value;
+}
+
+function subtract() {
+    value--;
+    number.innerHTML = value;
+}
+
+function clear() {
+    value = 0
+    number.innerHTML = value;
+}
